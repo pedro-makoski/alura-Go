36f0cc54f41111ac90dfb0afee799d8f3e765943
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2025-02-06 10:05:19 -0300

    Aula 02

---
+++ first-course/hello/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	nome := "Pedro Makoski"
+	idade := 15
+	versao := 1.1
+
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variável nome é ", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variável idade é ", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variável versao é ", reflect.TypeOf(versao))
+}
