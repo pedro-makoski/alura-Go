e7d3ebd7bf04cb2d7cc56efb7d0dae171696671c
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2025-02-06 11:35:19 -0300

    Capitulo 3

--- first-course/hello2/hello.go
+++ first-course/hello2/hello.go
@@ -2,33 +2,50 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	exibeIntroducao()
+
+	exibeMenu()
+
+	comando := getComando()
+
+	switch comando {
+		case 1:	
+			fmt.Println("Monitorando...")
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+	}
+}
+
+func exibeIntroducao(){
 	nome := "Pedro Makoski"
 	versao := 1.1
 
 	fmt.Println("Olá, sr(a).", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir os logs")
 	fmt.Println("0 - sair do programa")
+}
 
-	var comando int 
+func getComando() int {
+	var comandoLido int 
 	fmt.Print("Insira o comando: ")
-	fmt.Scan(&comando)
+	fmt.Scan(&comandoLido)
 
-	fmt.Println("O comando escolhido foi:", comando)
+	fmt.Println("O comando escolhido foi:", comandoLido)
 
-	switch comando {
-		case 1:	
-			fmt.Println("Monitorando...")
-		case 2, 7:
-			fmt.Println("Exibindo logs...")
-		case 0:
-			fmt.Println("Saindo do programa")
-		default:
-			fmt.Println("Não conheço este comando")
-	}
+	return comandoLido	
 }
