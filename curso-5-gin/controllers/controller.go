f8d17cb367fbfe76bd65aad50a534785dc7f4855
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-07-25 19:24:20 -0300

    Capitulo 3

--- curso-5-gin/controllers/controller.go
+++ curso-5-gin/controllers/controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/pedro-makoski/alura-Go/curso-5-gin/database"
 	"github.com/pedro-makoski/alura-Go/curso-5-gin/models"
 )
 
@@ -14,4 +17,14 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz:": "E ai " + nome + ", tudo beleza?",
 	})
+}
+
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
--- curso-5-gin/main.go
+++ curso-5-gin/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/pedro-makoski/alura-Go/curso-5-gin/models"
+	"github.com/pedro-makoski/alura-Go/curso-5-gin/database"
 	"github.com/pedro-makoski/alura-Go/curso-5-gin/routes"
 )
 
 func main() {
-	models.Alunos = []models.Aluno{
-		{"Pedro Makoski", "00000000000", "4700000000"},
-		{"Iksokam Ordep", "1234578912", "4300000000"},
-	}
+	database.ConectaComBancoDeDados()
 	routes.HandleRequests()
 }
