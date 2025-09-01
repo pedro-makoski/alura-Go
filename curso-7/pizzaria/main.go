4589360675ad2a823bbd59964a10f86e4b416260
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-09-01 17:58:51 -0300

    Capitulo 2 - Curso 7

--- curso-7/pizzaria/main.go
+++ curso-7/pizzaria/main.go
@@ -1,9 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Pizza struct {
+	ID    int
+	Nome  string
+	Preco float64
+}
 
 func main() {
-	nomePizzaria := "Pizzaria Go"
-	instagram, telefone := "@pizzariago", "4190"
-	fmt.Println(nomePizzaria, instagram, telefone)
+	router := gin.Default()
+
+	pizzas := []Pizza{
+		{
+			ID:    1,
+			Nome:  "Marguerita",
+			Preco: 30,
+		},
+		{
+			ID:    2,
+			Nome:  "Toscana",
+			Preco: 30,
+		},
+		{
+			ID:    3,
+			Nome:  "Atum Com Quiejo",
+			Preco: 29.1,
+		},
+	}
+
+	router.GET("/pizzas", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"pizzas": pizzas,
+		})
+	})
+
+	fmt.Println(pizzas)
+	router.Run(":8000")
 }
