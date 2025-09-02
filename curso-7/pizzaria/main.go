cce1533a1a21d89ba78c028c17b05cf4237dbd6b
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-09-02 18:23:49 -0300

    Capitulo 3 - Curso Go

--- curso-7/pizzaria/main.go
+++ curso-7/pizzaria/main.go
@@ -2,43 +2,40 @@ package main
 
 import (
 	"fmt"
+	"pizzaria/models"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Pizza struct {
-	ID    int
-	Nome  string
-	Preco float64
+var pizzas = []models.Pizza{
+	{
+		ID:    1,
+		Nome:  "Marguerita",
+		Preco: 30,
+	},
+	{
+		ID:    2,
+		Nome:  "Toscana",
+		Preco: 30,
+	},
+	{
+		ID:    3,
+		Nome:  "Atum Com Quiejo",
+		Preco: 29.1,
+	},
 }
 
 func main() {
 	router := gin.Default()
 
-	pizzas := []Pizza{
-		{
-			ID:    1,
-			Nome:  "Marguerita",
-			Preco: 30,
-		},
-		{
-			ID:    2,
-			Nome:  "Toscana",
-			Preco: 30,
-		},
-		{
-			ID:    3,
-			Nome:  "Atum Com Quiejo",
-			Preco: 29.1,
-		},
-	}
-
-	router.GET("/pizzas", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"pizzas": pizzas,
-		})
-	})
+	router.GET("/pizzas", GetPizzas)
 
 	fmt.Println(pizzas)
 	router.Run(":8000")
+}
+
+func GetPizzas(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
 }
