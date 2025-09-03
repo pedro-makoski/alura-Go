ea774426f72768c79510f2e3d99bfc12efa1131e
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-09-03 18:28:25 -0300

    Capitulo 4 - Curso 7

--- curso-7/pizzaria/main.go
+++ curso-7/pizzaria/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"pizzaria/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -29,6 +30,9 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/pizzas", GetPizzas)
+	router.GET("/pizzas/:id", GetPizzaById)
+
+	router.POST("/pizzas", PostPizzas)
 
 	fmt.Println(pizzas)
 	router.Run(":8000")
@@ -37,5 +41,41 @@ func main() {
 func GetPizzas(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"pizzas": pizzas,
+	})
+}
+
+func GetPizzaById(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"message": "Digite um número que faça sentido, não o texto: " + idParam,
+		})
+		return
+	}
+
+	for _, pizza := range pizzas {
+		if pizza.ID == id {
+			c.JSON(200, pizza)
+			return
+		}
+	}
+
+	c.JSON(404, gin.H{
+		"erro": "Pizza não encontrada",
+	})
+}
+
+func PostPizzas(c *gin.Context) {
+	var newPizza models.Pizza
+	if err := c.ShouldBindJSON(&newPizza); err != nil {
+		c.JSON(400, gin.H{"erro": err.Error()})
+		return
+	}
+
+	pizzas = append(pizzas, newPizza)
+	c.JSON(202, gin.H{
+		"item-criado": newPizza,
+		"foi-criado":  true,
 	})
 }
