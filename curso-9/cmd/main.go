c1804d75aabe6f6bae75b5a5e983a72e2dacd53e
Pedro Makoski <makoski.pedrogamail.com@gmail.com>
2025-10-24 18:55:28 -0300

    Capitulo 2 e 3 - Curso 8 - Go routines

--- curso-9/cmd/main.go
+++ curso-9/cmd/main.go
@@ -2,37 +2,21 @@ package main
 
 import (
 	"buscador/internal/fetcher"
+	"buscador/internal/processor"
 	"fmt"
-	"sync"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	var price1, price2, price3 float64
-	var wg sync.WaitGroup
-	wg.Add(3)
 
-	go func() {
-		defer wg.Done()
-		price1 = fetcher.FetchPriceFromSite1()
-	}()
+	priceChannel := make(chan float64)
+	done := make(chan bool)
 
-	go func() {
-		defer wg.Done()
-		price2 = fetcher.FetchPriceFromSite2()
-	}()
+	go fetcher.FetchPrices(priceChannel)
+	go processor.ShowPriceAvg(priceChannel, done)
 
-	go func() {
-		defer wg.Done()
-		price3 = fetcher.FetchPriceFromSite3()
-	}()
-
-	wg.Wait()
-
-	fmt.Printf("R$ %.2f\n", price1)
-	fmt.Printf("R$ %.2f\n", price2)
-	fmt.Printf("R$ %.2f\n", price3)
+	<-done
 
 	fmt.Printf("Tempo total: %s", time.Since(start))
 }
